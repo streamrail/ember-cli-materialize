05eb08641ca40e161b76e3de21de115793e1c168
Ramy Ben Aroya <ramybenaroya@gmail.com>
2017-01-16 17:46:32 +0200

    md-radios onChange

--- addon/components/md-radios.js
+++ addon/components/md-radios.js
@@ -1,6 +1,19 @@
 import SelectableItemGroup from './selectable-item-group';
+import afterRender from  '../utils/after-render';
 
 export default SelectableItemGroup.extend({
   classNames: ['md-radios'],
-  selectableItemView: 'md-radios-radio'
+  selectableItemView: 'md-radios-radio',
+  bindToInputs: afterRender(function(){
+  	this.$().on(`change.${this.get('elementId')}`, 'input[type="radio"]:checked', function(event){
+  		if (this.attrs.onChange) {
+  			this.attrs.onChange(this.value);
+  		}
+  	});
+  }),
+
+  willDestroyElement() {
+    this._super(...arguments);
+    this.$().off(`change.${this.get('elementId')}`);
+  }
 });
